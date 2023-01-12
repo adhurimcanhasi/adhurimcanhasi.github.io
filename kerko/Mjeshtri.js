3b16d803579e7432d92cfe2780fdf0325e94ec71
Adhurim Canhasi <adhurimc@gmail.com>
2023-01-12 17:38:57 +0100

    I cant seem to finish this project

--- kerko/Mjeshtri.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#ac-mobile-menu");
-const menuLinks = document.querySelector(".ac-navbar__menu");
-
-menu.addEventListener("click", function () {
-  menu.classList.toggle("is-active");
-  menuLinks.classList.toggle("active");
-});
